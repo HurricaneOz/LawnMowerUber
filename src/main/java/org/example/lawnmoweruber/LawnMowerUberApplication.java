933d32f5ad33faec092593a4e5ee0bba94c58259
HurricaneOz <okha1547@gmail.com>
2025-08-25 14:58:06 -0500

    initial commit

---
+++ src/main/java/org/example/lawnmoweruber/LawnMowerUberApplication.java
@@ -0,0 +1,13 @@
+package org.example.lawnmoweruber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawnMowerUberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawnMowerUberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lawnmoweruber/LawnMowerUberApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lawnmoweruber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawnMowerUberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
